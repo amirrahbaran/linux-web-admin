b246541e8e736e4021b9580c4609ebaa72396f74
Amir Reza Rahbaran <amirrezarahbaran@gmail.com>
2017-01-16 11:39:29 +0330

    Writes each link config including sub interfaces at one complex file and change shutdown/startup method application logic

--- main/views.py
+++ main/views.py
@@ -1,9 +1,9 @@
+import re
 import os
 import json
-from time import sleep
 
 from main.file import File
-from main.networking import Ethernet
+from main.networking import NetworkInterface
 from main.nspath import NETWORK_CONF_PATH
 
 
@@ -22,90 +22,95 @@ def send_request(method_value="POST", url_value="", fields_value=None, headers_v
             'Record': None
         }))
 
+
+def startup(TheInterface):
+    if not TheInterface.status:
+        return False
+    TheInterfaceMainName = TheInterface.name
+    ethernet_filename = "ifcfg-" + TheInterfaceMainName
+    ethernet_file_object = File(ethernet_filename, NETWORK_CONF_PATH)
+    if not ethernet_file_object.Existed():
+        return False
+    for line in ethernet_file_object.Read().split("\n"):
+        match = re.search('^iface\s+(\S+)\s+inet', line)
+        if match:
+            TheInterfaceName = match.group(1)
+            ethernet_object = NetworkInterface(TheInterfaceName)
+            ethernet_object.Up()
+            ethernet_object.ifUp()
+    return True
+
+
 def shutdown(TheInterface):
-    TheInterfaceName = TheInterface.name
-    IPv4AddressList = TheInterface.ipv4address.split(",")
-    if len(IPv4AddressList) > 1:
-        for IPv4AddressCounter in range(len(IPv4AddressList)):
-            if (IPv4AddressCounter > 0):
-                TheInterfaceName += (":" + (str(IPv4AddressCounter - 1)))
-            ethernet_object = Ethernet(TheInterfaceName)
+    TheInterfaceMainName = TheInterface.name
+    ethernet_filename = "ifcfg-" + TheInterfaceMainName
+    ethernet_file_object = File(ethernet_filename, NETWORK_CONF_PATH)
+    if not ethernet_file_object.Existed():
+        return False
+    for line in ethernet_file_object.Read().split("\n"):
+        match = re.search('^iface\s+(\S+)\s+inet', line)
+        if match:
+            TheInterfaceName = match.group(1)
+            ethernet_object = NetworkInterface(TheInterfaceName)
+            ethernet_object.ifDown()
             ethernet_object.Down()
-    else:
-        ethernet_object = Ethernet(TheInterfaceName)
-        ethernet_object.Down()
+    return True
 
 
 def removeNetworkConfigurationOf(TheInterface):
-    TheInterfaceName = TheInterface.name
-    IPv4AddressList = TheInterface.ipv4address.split(",")
-    if len(IPv4AddressList) > 1:
-        for IPv4AddressCounter in range(len(IPv4AddressList)):
-            if ( IPv4AddressCounter > 0 ):
-                TheInterfaceName += (":" + (str(IPv4AddressCounter - 1)))
-            ethernet_filename = "ifcfg-" + TheInterfaceName
-            ethernet_file_object = File(ethernet_filename, NETWORK_CONF_PATH)
-            ethernet_file_object.Remove()
-    else:
-        ethernet_filename = "ifcfg-" + TheInterfaceName
-        ethernet_file_object = File(ethernet_filename, NETWORK_CONF_PATH)
-        ethernet_file_object.Remove()
+    TheInterfaceMainName = TheInterface.name
+    ethernet_filename = "ifcfg-" + TheInterfaceMainName
+    ethernet_file_object = File(ethernet_filename, NETWORK_CONF_PATH)
+    ethernet_file_object.Remove()
 
 
 def setNetworkConfigurationOf(TheInterface):
     if not TheInterface.status:
         return False
-    TheInterfaceName = TheInterface.name
-    if not TheInterface.dhcp:
-        IPv4AddressList = TheInterface.ipv4address.split(",")
-        IPv4AddressCounter = 0
-        for eachIpv4Address in IPv4AddressList:
-            isVirtual = False
-            ConfigurationsText = ""
-            if ( IPv4AddressCounter > 0 ):
-                TheInterfaceName += (":" + (str(IPv4AddressCounter - 1)))
-                isVirtual = True
-            ipv4address_with_cidr = eachIpv4Address.split("/")
+    TheInterfaceMainName = TheInterface.name
+    IPv4AddressList = TheInterface.ipv4address.split(",")
+    IPv4AddressCounter = 0
+    ConfigurationsText = ""
+    for eachIpv4Address in IPv4AddressList:
+        isVirtual = False
+        if ( IPv4AddressCounter > 0 ):
+            TheInterfaceName = TheInterfaceMainName + (":" + (str(IPv4AddressCounter - 1)))
+            isVirtual = True
+        else:
+            TheInterfaceName = TheInterfaceMainName
+        ipv4address_with_cidr = eachIpv4Address.split("/")
 
-            if TheInterface.status:
-                ConfigurationsText += "auto " + TheInterfaceName + "\n"
+        if TheInterface.status:
+            ConfigurationsText += "auto " + TheInterfaceName + "\n"
 
-            ConfigurationsText += "iface " + TheInterfaceName + " inet "
+        ConfigurationsText += "iface " + TheInterfaceName + " inet "
 
-            if TheInterface.dhcp:
-                ConfigurationsText += "dhcp\n"
+        if TheInterface.dhcp:
+            ConfigurationsText += "dhcp\n"
+        else:
+            ConfigurationsText += "static\n"
+            ConfigurationsText += "\taddress " + ipv4address_with_cidr[0] + "\n"
+            if len(ipv4address_with_cidr) > 1:
+                ConfigurationsText += "\tnetmask " + ipv4address_with_cidr[1] + "\n"
             else:
-                ConfigurationsText += "static\n"
-                ConfigurationsText += "\taddress " + ipv4address_with_cidr[0] + "\n"
-                if len(ipv4address_with_cidr) > 1:
-                    ConfigurationsText += "\tnetmask " + ipv4address_with_cidr[1] + "\n"
-                else:
-                    ConfigurationsText += "\tnetmask 32\n"
-                if not isVirtual:
-                    if TheInterface.gateway != "":
-                        ConfigurationsText += "\tgateway " + TheInterface.gateway + "\n"
-
+                ConfigurationsText += "\tnetmask 32\n"
             if not isVirtual:
-                ConfigurationsText += "\thwaddress " + TheInterface.mac + "\n"
-                if (TheInterface.mtu != "1500"):
-                    ConfigurationsText += "\tmtu " + TheInterface.mtu + "\n"
+                if TheInterface.gateway != "":
+                    ConfigurationsText += "\tgateway " + TheInterface.gateway + "\n"
 
-                if TheInterface.manual_dns:
-                    nameservers = TheInterface.dnsserver.replace(",", " ")
-                    ConfigurationsText += "\tdns-nameservers " + nameservers
-                    ConfigurationsText += "\n"
+        if not isVirtual:
+            if (TheInterface.mtu != "1500"):
+                ConfigurationsText += "\tmtu " + TheInterface.mtu + "\n"
 
-            ethernet_filename = "ifcfg-" + TheInterfaceName
-            ethernet_file_object = File(ethernet_filename, NETWORK_CONF_PATH)
-            ethernet_file_object.Write(ConfigurationsText)
-            IPv4AddressCounter += 1
-            ethernet_object = Ethernet(TheInterfaceName)
-            if TheInterface.status:
-                ethernet_object.Down()
-                sleep(1)
-                ethernet_object.Up()
-            else:
-                ethernet_object.Down()
+            if TheInterface.manual_dns:
+                nameservers = TheInterface.dnsserver.replace(",", " ")
+                ConfigurationsText += "\tdns-nameservers " + nameservers
+                ConfigurationsText += "\n"
+        ConfigurationsText += "\n"
+        IPv4AddressCounter += 1
+    ethernet_filename = "ifcfg-" + TheInterfaceMainName
+    ethernet_file_object = File(ethernet_filename, NETWORK_CONF_PATH)
+    return ethernet_file_object.Write(ConfigurationsText)
 
 
 def removeRoutingConfigurationOf(TheRoute):
